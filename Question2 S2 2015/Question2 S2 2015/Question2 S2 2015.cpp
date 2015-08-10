e76c1a5fa23dca095e1aa77c27d3cee9dd699b43
Leon Nortje <leon.nortje@outlook.com>
2015-08-10 21:45:27 +0200

    Update

---
+++ Question2 S2 2015/Question2 S2 2015/Question2 S2 2015.cpp
@@ -0,0 +1,17 @@
+// Question2 S2 2015.cpp : Defines the entry point for the console application.
+// Author: Leon Nortje
+// Email Address: nortje.leon@gmail.com
+// Assignment Number: 2
+// Description: using assert function.
+// Last Changed: August 201
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    return 0;
+}
+
