90fa927fadeacb0dabbf9a182219e9301484eaae
Leon Nortje <leon.nortje@outlook.com>
2015-08-11 17:29:28 +0200

    Update

--- Question2 S2 2015/Question2 S2 2015/Question2 S2 2015.cpp
+++ Question2 S2 2015/Question2 S2 2015/Question2 S2 2015.cpp
@@ -7,11 +7,31 @@
 
 #include "stdafx.h"
 #include <iostream>
+#include <cassert>
 using namespace std;
 
 
 int main()
 {
-    return 0;
+	int mathematics, physical_science, english;
+
+	cout << "Please enter your marks for the follow subjects to determine if you qualify for Computer Science.\n";
+	cout << "Matematics:\n";
+	cin >> mathematics;
+	cout << "Physical Science:\n";
+	cin >> physical_science;
+	cout << "English:\n";
+	cin >> english;
+
+	//Add this as a function call
+	assert(mathematics > 100) || (physical_science > 100) || (english > 100);
+	while (((mathematics >= 60) && (physical_science >= 60) && (english >= 60)))
+	{
+		cout << "Congrats!";
+	}
+
+		cout << "You dont qualify!";
+	
+	return 0;
 }
 
