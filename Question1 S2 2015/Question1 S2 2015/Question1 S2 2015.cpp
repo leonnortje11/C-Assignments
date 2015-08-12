74b89fd799d2bebdf4fa79543aa242f751598c10
Leon Nortje <leon.nortje@outlook.com>
2015-08-12 22:55:44 +0200

    Update

--- Question1 S2 2015/Question1 S2 2015/Question1 S2 2015.cpp
+++ Question1 S2 2015/Question1 S2 2015/Question1 S2 2015.cpp
@@ -9,10 +9,88 @@
 #include <iostream>
 using namespace std;
 
+double calcWeeklyPay(double annual_salary_par);
+//Calculates the weekly pay based on Salaried employees or hourly paid employees
+
+double calcWeeklyPay(double hourly_rate_par, int hours_worked_par);
+//Calculates the weekly pay based on Salaried employees or hourly paid employees
 
 int main()
 {
     
+	double weekly_pay, annual_salary, hourly_rate;
+	int hours_worked;
+	char selection;
+
+	cout << "Please indicate if you are a salary 'S' or Hourly Paid employee 'H'\n";
+	cin >> selection;
+
+	switch (selection)
+	{
+	case 'S':
+		cout << "Please enter your annual salary: ";
+		cin >> annual_salary;
+		weekly_pay = calcWeeklyPay(annual_salary);
+		cout << "Your weekly pay will be R " << weekly_pay;
+		break;
+
+	case 's':
+		cout << "Please enter your annual salary: ";
+		cin >> annual_salary;
+		weekly_pay = calcWeeklyPay(annual_salary);
+		cout << "Your weekly pay will be R " << weekly_pay;
+		break;
+
+
+	case 'H':
+		cout << "Please enter your hourly rate: ";
+		cin >> hourly_rate;
+		cout << "Please enter your hours worked for the week: ";
+		cin >> hours_worked;
+		weekly_pay = calcWeeklyPay(hourly_rate, hours_worked);
+		cout << "Your weekly pay will be R " << weekly_pay;
+		break;
+
+	case 'h':
+		cout << "Please enter your hourly rate: ";
+		cin >> hourly_rate;
+		cout << "Please enter your hours worked for the week: ";
+		cin >> hours_worked;
+		weekly_pay = calcWeeklyPay(hourly_rate, hours_worked);
+		cout << "Your weekly pay will be R " << weekly_pay;
+		break;
+	default:
+		cout << "That was not a required selection\n";
+	}
+
 	return 0;
 }
 
+double calcWeeklyPay(double annual_salary_par)
+{
+	int weeks;
+	double weekly_pay;
+
+	weeks = 52;
+	weekly_pay = annual_salary_par / weeks;
+
+	cout.setf(ios::fixed);
+	cout.setf(ios::showpoint);
+	cout.precision(2);
+
+	return (weekly_pay);
+}
+
+double calcWeeklyPay(double hourly_rate_par, int hours_worked_par)
+{
+	
+	double weekly_pay;
+
+	weekly_pay = hourly_rate_par * hours_worked_par;
+
+	cout.setf(ios::fixed);
+	cout.setf(ios::showpoint);
+	cout.precision(2);
+
+	return (weekly_pay);
+}
