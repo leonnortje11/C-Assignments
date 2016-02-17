f712e55a7f8545d0f97c01905653d557e2e47369
leonnortje11 <nortje.leon@gmail.com>
2016-02-17 22:05:27 +0200

    ok

---
+++ New/New/main.cpp
@@ -0,0 +1,15 @@
+//
+//  New.cpp
+//  New
+//
+//  Created by Leon Nortje on 2016/02/17.
+//  Copyright © 2016 Leon Nortje. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
