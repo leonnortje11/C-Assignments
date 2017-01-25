7adf819b04a837dd9ff2595d3a9d7f3ec37cf33b
leonnortje11 <leon.nortje@outlook.com>
2017-01-25 06:53:14 +0200

    UNISA 2017

---
+++ Question2 S1 2017/Question2 S1 2017/Question2 S1 2017.cpp
@@ -0,0 +1,18 @@
+// Question2 S1 2017.cpp : Defines the entry point for the console application.
+// Author: Leon Nortje
+// Email Address: leon.nortje@outlook.com
+// Assignment Number: 1 - 2017
+// Description: Writing a program that will calculate the circumference of a figure
+// Last Changed:
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    return 0;
+}
+
