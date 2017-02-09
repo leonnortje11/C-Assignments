9f130ac0caaa238198e7ad028e47bb5e42646edf
leonnortje11 <nortje.leon@gmail.com>
2017-02-09 22:30:21 +0200

    ok from Mac

---
+++ Question3 S1 2017/Question3 S1 2017/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  Question3 S1 2017
+//
+//  Created by Leon Nortje on 2017/02/09.
+//  Copyright © 2017 Leon Nortje. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // insert code here...
+    cout << "Hello, World!\n";
+    return 0;
+}
