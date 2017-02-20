cafb6c986b9808f5a213b4fe25d12b662254ad82
leonnortje11 <leon.nortje@outlook.com>
2017-02-20 20:46:13 +0200

    Update

--- Question 3 S1 2017/Question 3 S1 2017/Question 3 S1 2017.cpp
+++ Question 3 S1 2017/Question 3 S1 2017/Question 3 S1 2017.cpp
@@ -5,6 +5,7 @@
 
 #include "stdafx.h"
 #include <iostream>
+#include <cassert>
 
 using namespace std;
 
@@ -15,24 +16,63 @@ int main()
 	int daysPerMonths[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 	int day1, day2;
 	int month1, month2;
-	int n;
-	int julian;
-	n = 0;
+	int julian1, julian2, juliandiff;
+	
+	julian1 = 0;
+	julian2 = 0;
 
-	cout << "Please enter day 1: ";
+
+	cout << "Please enter day1: ";
 	cin >> day1;
 	cout << "Please enter month1: ";
 	cin >> month1;
+	month1 = month1 - 1;
+	assert(day1 <= daysPerMonths[month1]);
+	cout << endl;
 
-	for (n = 0; n<=3; n++)
-		julian = julian + n;
-	cout << julian;
+	while (!month1 == 0)
+	{
+		julian1 = julian1 + daysPerMonths[month1];
+		month1--;
+	}
+	
+	julian1 = julian1 + day1;
+	cout << "The first julian date is: " << julian1 << endl;
 
-	cout << "Please enter day 2: ";
+	cout << endl;
+
+	cout << "Please enter day2: ";
 	cin >> day2;
 	cout << "Please enter month2: ";
 	cin >> month2;
+	month2 = month2 - 1;
+	assert(day2 <= daysPerMonths[month2]);
 
+	
+	cout << endl;
+
+
+	while (!month2 == 0)
+	{
+		julian2 = julian2 + daysPerMonths[month2];
+		month2--;
+	}
+
+	julian2 = julian2 + day2;
+	cout << "The second julian date is: " << julian2 << endl;
+
+	if (julian1 > julian2)
+	{
+		juliandiff = julian1 - julian2;
+		cout << "The julian differance = " << juliandiff;
+	}
+	else
+	{
+		juliandiff = julian2 - julian1;
+		cout << "The julian differance = " << juliandiff;
+	}
+	
+	cout << endl;
 
 	system("pause");
 	
