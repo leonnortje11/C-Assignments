3457325f5b30ddb2d6b1acc2cdb1d596d7b73002
Leon Nortje <leon.nortje@media24.com>
2017-07-16 11:57:41 +0200

    OK

--- Question 2 S2 2017/Question 2 S2 2017/Question2S22017.cpp
+++ Question 2 S2 2017/Question 2 S2 2017/Question2S22017.cpp
@@ -1,7 +1,7 @@
 // Question2S22017.cpp : Defines the entry point for the console application.
 //	Created by Leon Nortje
 //  Email Address: leon.nortje@outlook.com
-//  Description: assert function - 
+//  Description: assert function - Write a program that determines the difference between two times in 24H00 format - assert macro
 
 #include "stdafx.h"
 #include <iostream>
