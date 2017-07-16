63aa272f798274e7020e4466f56136c48fed25f9
Leon Nortje <leon.nortje@media24.com>
2017-07-16 11:55:56 +0200

    OK

---
+++ Question 2 S2 2017/Question 2 S2 2017/Question2S22017.cpp
@@ -0,0 +1,20 @@
+// Question2S22017.cpp : Defines the entry point for the console application.
+//	Created by Leon Nortje
+//  Email Address: leon.nortje@outlook.com
+//  Description: assert function - 
+
+#include "stdafx.h"
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+
+int main()
+{
+    
+	system("pause");
+
+	return 0;
+}
+
