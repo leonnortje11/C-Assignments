15582f492b47037940e587b4889066dd2a0b0ecb
leonnortje11 <leon.nortje@outlook.com>
2017-07-29 20:28:34 +0200

    OK

---
+++ Question 1 S2 2017 XCODE/Question 1 S2 2017 XCODE/main.cpp
@@ -0,0 +1,18 @@
+// Question1S22017.cpp : Defines the entry point for the console application.
+// Author: Leon Nortje
+// Email Address: leon.nortje@outlook.com
+// Assignment Number: 1 - 2017
+// Description: Write a program to determine whether a ticket number is a winning number, and calculate the cash prize for the ticket number
+// Last Changed:
+
+//#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello world\n";
+    
+    //system("pause");
+    return 0;
+}
